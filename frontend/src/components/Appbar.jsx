3638a9647edd5e068037b4c98d573d1c84417243
MethuParoi <paroimethu@gmail.com>
2024-02-03 23:34:28 +0530

    frontend updated

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -10,9 +10,9 @@ export const Appbar = () => {
   const name = useRecoilValue(usernameAtom);
 
   return (
-    <div className="shadow h-14 flex justify-between my-2 px-4">
-      <div className="flex flex-col justify-center h-full ml-4">
-        <p className="text-xl lg:text-2xl font-semibold">PayTM App</p>
+    <div className="shadow h-14 flex justify-between my-2 lg:px-4 px-2">
+      <div className="flex flex-col justify-center h-full lg:ml-4 ml-2">
+        <p className="text-lg lg:text-2xl font-semibold">PayTM App</p>
       </div>
       <div className="flex items-center">
         <div className="invisible lg:visible text-xl flex flex-col justify-center h-full mr-4">
@@ -21,7 +21,7 @@ export const Appbar = () => {
 
         <div>
           <button
-            className="text-lg lg:text-xl py-2 px-4 bg-slate-200 rounded-lg"
+            className="text-lg lg:text-xl lg:py-2 py-1.5 px-2.5 lg:px-4 bg-slate-200 rounded-lg"
             onClick={() => {
               //remove user from local storage
               localStorage.removeItem("token");
