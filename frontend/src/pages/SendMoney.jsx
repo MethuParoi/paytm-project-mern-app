5a38b4cdabea3c9891ffdced53d93ea03b8d3167
MethuParoi <paroimethu@gmail.com>
2024-02-04 00:16:07 +0530

    frontend updated

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -18,7 +18,7 @@ export const SendMoney = () => {
   return (
     <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
-        <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
+        <div className="border h-min text-card-foreground lg:max-w-md max-w-[300px] p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
           <div className="flex flex-col space-y-1.5 p-6">
             <h2 className="text-3xl font-bold text-center">Send Money</h2>
           </div>
